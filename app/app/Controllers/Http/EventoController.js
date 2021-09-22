4039028adc5d267b96a76360417d3a147e932aa4
Jonas Pohlmann Araujo <jonasparaujo852@gmail.com>
2021-09-22 00:09:22 -0300

    tratamento de erros e ajuste no login

--- app/app/Controllers/Http/EventoController.js
+++ app/app/Controllers/Http/EventoController.js
@@ -1,8 +1,12 @@
 'use strict'
 
+const LoginController = require('./LoginController')
+
 class EventoController {
     
-    async listarEventos({view}) {
+    async listarEventos({view, session, response}) {
+        LoginController.logado(session, response)
+        
         return view.render('evento')
     }
 
