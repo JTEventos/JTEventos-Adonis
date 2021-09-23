fff0fc3323f2ab1e73b5ce378661fd4f80745062
Jonas Pohlmann Araujo <jonasparaujo852@gmail.com>
2021-09-22 21:55:00 -0300

    início do crud

--- ddl.sql
+++ ddl.sql
@@ -1,44 +1,43 @@
-DROP TABLE IF EXISTS USUARIO;
-DROP TABLE IF EXISTS EVENTO;
-DROP TABLE IF EXISTS CLIENTE;
+drop table if exists usuarios;
+drop table if exists eventos;
+drop table if exists clientes;
 
-CREATE TABLE USUARIO (
-    ID int not null auto_increment,
-    NOME varchar(50),
-    USUARIO varchar(50) not null unique,
-    SENHA varchar(100) not null ,
-PRIMARY KEY (ID)
+create table usuarios (
+    id int not null auto_increment,
+    nome varchar(50),
+    usuario varchar(50) not null unique,
+    senha varchar(100) not null ,
+primary key (id)
 );
  
-CREATE TABLE CLIENTE (
-    ID int not null auto_increment,
-    NOME varchar(100) not null,
-    CPF varchar(20) not null unique,
-    RG varchar(20),
-    CEP varchar(15),
-    LOGRADOURO varchar(100),
-    NUMERO varchar(10),
-    COMPLEMENTO varchar(20),
-    BAIRRO varchar(50),
-    CIDADE varchar(50),
-    ESTADO varchar(2),
-    EMAIL varchar(100),
-    TEL_CELULAR varchar(15),
-    TEL_FIXO varchar(15),
-PRIMARY KEY (ID)
+create table clientes (
+    id int not null auto_increment,
+    nome varchar(100) not null,
+    cpf varchar(20) not null unique,
+    rg varchar(20),
+    cep varchar(15),
+    logradouro varchar(100),
+    numero varchar(10),
+    complemento varchar(20),
+    bairro varchar(50),
+    cidade varchar(50),
+    estado varchar(2),
+    email varchar(100),
+    tel_celular varchar(15),
+    tel_fixo varchar(15),
+primary key (id)
 );
 
  
-CREATE TABLE EVENTO /*(mandar por e-mail a confirmação do evento)*/ (
-    ID int not null auto_increment,
-    ID_CLIENTE int,
-    NOME_EVENTO varchar(100),
-    DATA_INICIO datetime,
-    DATA_FIM datetime,
-    LISTA_CONVIDADOS varchar(4000),
-PRIMARY KEY (ID),
-CONSTRAINT FK_EventoCliente FOREIGN KEY (ID_CLIENTE) REFERENCES CLIENTE(ID)
+create table eventos /*(mandar por e-mail a confirmação do evento)*/ (
+    id int not null auto_increment,
+    id_cliente int,
+    nome_evento varchar(100),
+    data_inicio datetime,
+    data_fim datetime,
+    lista_convidados varchar(4000),
+primary key (id),
+constraint fk_eventocliente foreign key (id_cliente) references clientes(id)
 );
 
-
-INSERT INTO usuario (NOME, USUARIO, SENHA) VALUES ('Administrador', 'admin', '12345');
+insert into usuarios (nome, usuario, senha) values ('Administrador', 'admin', '12345');
